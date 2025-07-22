bd66f4ee35b8205a41af928d10c01808827207e4
Smooth4life <129492868+Smooth4life@users.noreply.github.com>
2025-07-22 14:14:04 +0900

    11

--- Source/Wizard_Dormitory/Private/NPC.cpp
+++ Source/Wizard_Dormitory/Private/NPC.cpp
@@ -119,8 +119,8 @@ void ANPC::ApplyVisual(const FNPCVisualData& VisualData)
 	if (EyeMaterialInstance)
 	{
 		
-		EyeMaterialInstance->SetVectorParameterValue("EyeUV",
-			FLinearColor(VisualData.FaceData.EyeUV.X, VisualData.FaceData.EyeUV.Y, 0, 0));
+		//EyeMaterialInstance->SetVectorParameterValue("EyeUV",
+			//FLinearColor(VisualData.FaceData.EyeUV.X, VisualData.FaceData.EyeUV.Y, 0, 0));
 		//UE_LOG(LogTemp, Warning, TEXT("Setting EyeUV: X=%f, Y=%f"), VisualData.FaceData.EyeUV.X, VisualData.FaceData.EyeUV.Y);
 		//UE_LOG(LogTemp, Warning, TEXT("Slot 1 Material: %s"), *GetMesh()->GetMaterial(1)->GetName());
 
